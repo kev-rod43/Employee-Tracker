30991b0f228cef6538c822f2f66af75249b680ea
Kev-rod43 <kev.rod43@yahoo.com>
2024-02-05 16:37:51 -0500

    added dependencies, modified schema to fix syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,27 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department(
-    id INT SET PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT SET PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2),
-    department_id INT FOREIGN KEY REFERENCES department(id), 
+    department_id INT,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id) 
 );
 
 CREATE TABLE employee(
-    id INT SET PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT FOREIGN KEY REFERENCES role(id),
-    manager_id INT FOREIGN KEY REFERENCES employee(id),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES role(id),
+    FOREIGN KEY(manager_id)
+    REFERENCES employee(id)
 );
